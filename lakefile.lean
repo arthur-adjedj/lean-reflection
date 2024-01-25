cc273f088516c59fa392fa9be40c7bb56b831c24
Kiiya <snoewflaek@gmail.com>
2024-01-25 22:12:27 +0100

    update lean version

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,10 @@ package «reflection» {
   -- add package configuration options here
 }
 
+-- require aesop from git "https://github.com/leanprover-community/aesop" -- 01cb707de69cf88733addfff0bacc710efae8d20
+require std from git "https://github.com/leanprover/std4" @ "v4.5.0-rc1"
+require aesop from git "https://github.com/JLimperg/aesop" @ "v4.5.0-rc1"
+
 @[default_target]
 lean_lib «Reflection» {
   -- add library configuration options here
