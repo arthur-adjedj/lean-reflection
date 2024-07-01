0592086db33cb6a0b149e790077bf5544be416dd
Kiiya <max.nowak.j@gmail.com>
2024-07-01 16:45:04 +0200

    some work on term lowering

--- Reflection/IndexErasure/LowerTerms.lean
+++ Reflection/IndexErasure/LowerTerms.lean
@@ -1,4 +1,5 @@
 import Reflection.MInd
+import Reflection.MInd.Derive
 import Reflection.IndexErasure.Erase
 import Reflection.IndexErasure.Guard
 import Reflection.IndexErasure.Lower
@@ -15,51 +16,53 @@ open Tyₛ Tyₚ Varₛ Varₚ
 -/
 
 open Lean Elab Term Meta
--- open Qq
+open Derive Qq
 
+/-- Lowers a definition declaration. Expects `e` to be of form `Expr.const ..`,
+  *without args*, since monomorphization isn't handled here by index erasure. -/
 def lowerDef (e : Expr) : MetaM Expr := do
   sorry
 
-def lowerInduct (e : Expr) : MetaM Expr := do
-  let Γₛ : Expr := sorry
-  let Γₚ : Expr := sorry
+#check @Derive.getConₛ
 
-  sorry
-  -- let ΓₚE : Q(Conₚ $ΓₛE) := mkApp2 q(@eConₚ.{0}) Γₛ Γₚ
-  -- let γₛE : Q(EConₛA.{0,0} $Γₛ)      := mkApp2 q(@mkConₛ.{0}) ΓₛE ΓₚE
-  -- let γₚE : Q(EConₚA.{0,0} $Γₚ $γₛE) := mkApp2 q(@mkConₚ.{0}) ΓₛE ΓₚE
+/-- Lowers an inductive type to the product of the type-index erased version and its guard.
+  For example maps `Vec` to `fun (n : Nat) => (e : VecE) × VecG n e`.
+  Works with mutual inductive blocks.  -/
+def lowerTmₛ {Aₛ : Q(Tyₛ.{u})} (T : Q(TyₛA.{u, u} $Aₛ)) : MetaM Q(TyₛA.{u, u+2} $Aₛ) := do
+  let .const n _lvls := T.getAppFn | throwError "lowerTmₛ: T isn't an app of a const"
+  let ii <- getConstInfoInduct n
+  let u <- mkFreshLevelMVar
+  let Ωₛ : Q(Conₛ.{u}) <- getConₛ (u := u) ii
+  let Ωₚ : Q(Conₚ.{u} $Ωₛ) <- getConₚ (u := u) ii Ωₛ
+  let T : Q(Tmₛ $Ωₛ $Aₛ) <- getTmₛ ii.all Ωₛ Aₛ T
+  let L := q(mkLTyₛ.{u} $Ωₛ $Ωₚ $T)
+  return L
 
-  -- let ΓₛG : Q(Conₛ.{0}) := mkApp2 q(@gConₛ.{0}) Γₛ γₛE
-  -- let ΓₚG : Q(Conₚ $ΓₛG) := mkApp4 q(@gConₚ) Γₛ γₛE Γₚ γₚE
-  -- let γₛG /- : Q(GConₛA.{0,0} ..) -/ := mkApp2 q(@mkConₛ.{0}) ΓₛG ΓₚG
-  -- let γₚG /- : Q(GConₚA.{0,0} ..)-/ := mkApp2 q(@mkConₚ.{0}) ΓₛG ΓₚG
-  -- -- let L : Q(Type) := q(@Sigma )
--- #exit
+elab "asdf" : term => lowerTmₛ (u := 0) (Aₛ := q(.U)) q(Derive.Vec 132)
+def chk : Type 2 := asdf
+#print chk
+#reduce chk
 
-/-- Given `Vec 123`, produces `VecL 123`. -/
-def lowerTmₛ (e : Expr) : MetaM Expr := do
-  sorry
-
-/-- Given `Vec.cons`, produces `VecL.cons`. -/
+/-- Given the type of `Vec.cons`, produces the type of `VecL.cons`. -/
 def lowerTyₚ (e : Expr) : MetaM Expr := do
   sorry
 
 /-- Lowers a term.  -/
-def lowerTm (e : Expr) : MetaM Expr := do
+def lowerTmₚ (e : Expr) : MetaM Expr := do
   match e with
   | .app f a =>
     -- `⊢ f : Pi A B`
     -- `⊢ a : A`
     -- `f a : B[a]`
     -- `⊢ fᴸ : Pi Aᴸ Bᴸ`
-    return .app (<- lowerTm f) (<- lowerTm a)
+    return .app (<- lowerTmₚ f) (<- lowerTmₚ a)
   -- | .lam .. => sorry
-  | .const name _ =>
-    match <- getConstInfo name with
-    | .inductInfo _ => lowerInduct e
-    | .ctorInfo _ => lowerTyₚ e
-    | .defnInfo _ => lowerDef e
-    | _ => throwError "lowerEnv: unsupported const kind for {name}"
+  -- | .const name _ =>
+  --   match <- getConstInfo name with
+  --   | .inductInfo _ => lowerTmₛ e
+  --   | .ctorInfo _ => lowerTyₚ e
+  --   | .defnInfo _ => lowerDef e
+  --   | _ => throwError "lowerEnv: unsupported const kind for {name}"
   | _ => throwError "oh no, lowerTm {e}"
 
 /-- Lowers a type. `lower : Ty Γ -> Ty Γᴸ`
@@ -71,13 +74,15 @@ def lowerTy : {Γ : Con} -> Ty Γ -> Ty Γᴸ
   .Pi AL BL -- typechecks
 | Γ, El T => sorry
 ``` -/
-partial def lowerTy (u : Level) (e : Expr) : MetaM Expr := do
+partial def lowerTy (u : Level) (e : Q(Type u)) : MetaM Q(Type u) := do
   if let Expr.sort _ := e then -- case `U : Ty Γ`
     return e
   else if let .forallE _ A _ _ := e then -- case `Pi A B : Ty Γ`
+    let uA <- mkFreshLevelMVar
+    let A : Q(Type $uA) := A
     Meta.forallBoundedTelescope e (some 1) fun a_fv B => do
       let Γ_A <- getLCtx -- `Γ, A` and `Γ, A ⊢ B`
-      let AL : Expr <- lowerTy u A -- Γᴸ ⊢ Aᴸ
+      let AL : Q(Type $uA) <- lowerTy uA A -- Γᴸ ⊢ Aᴸ
       let BL : Expr <- lowerTy u B -- (Γ, A)ᴸ ⊢ Bᴸ    is (hopefully) well-typed again.
       let Γ_AL := Γ_A.modifyLocalDecl a_fv[0]!.fvarId! (fun ldecl => ldecl.setType AL)
       withLCtx Γ_AL (<- getLocalInstances) do
@@ -86,26 +91,24 @@ partial def lowerTy (u : Level) (e : Expr) : MetaM Expr := do
     -- `⊢ a : A` gets casted to `⊢ aL : Aᴸ`. Since metaprogramming is untyped, this is a no-op.
     return .fvar fv
   else -- case `El T : Ty Γ`
-    lowerTm e
-    -- let T := e.getAppFn
-    -- let args := e.getAppArgs
-    -- let v <- mkFreshLevelMVar
-    -- if <- isDefEq T (.const ``Vec [v]) then
-    --   let n := args[1]!
-    --   return mkAppN (.const ``Sigma [u, u]) #[.const ``Example.VecE [u], .app (.const ``Example.VecG [u]) n]
-    -- else if T.isAppOf ``Eq then return mkAppN (.const ``Eq [.zero]) args
-    -- else if T.isAppOf ``Nat || T.isAppOf ``String then return e
-    -- else throwError "oh no, {e}"
+    let Aₛ <- mkFreshExprMVarQ q(Tyₛ.{u})
+    lowerTmₛ (u := u) (Aₛ := Aₛ) e
 
--- open Example
-@[irreducible] def len  : Vec String n -> Nat := fun _ => n
+@[irreducible] def len : Vec String n -> Nat := fun _ => n
 
 elab "lower! " T:term : term => do
   let T <- elabTerm T none
   let u <- Meta.mkFreshLevelMVar
   lowerTy (u := u) T
 
-#check ∀n, ∀x, lower! ∀v, len (Vec.cons n x v) = (len v) + 1
-/- (n : Nat) → (x : String) → (v : PSigma (VecG n)) → TEq (len (Vec.cons n x v)) (len v + 1) -/
+def test₁ := lower! Derive.Vec 123
+#print test₁
 
--- #eval lowerTy q((v : MutualInductive.Vec String (nat_lit 0)) -> TEq (len (Vec.cons 0 "a" v)) ((len v) + 1))
+def test₂ := lower! Derive.Vec
+#print test₂
+
+#check ∀n, ∀x, lower! ∀v, len (Vec.cons n x v) = (len v) + 1
+
+  -- indexErase
+  -- mono
+  -- cvc5
