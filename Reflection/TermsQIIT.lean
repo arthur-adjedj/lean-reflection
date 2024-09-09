9946c459d7745c45c8d3162f00364a257404349e
Kiiya <max.nowak.j@gmail.com>
2024-09-09 16:09:38 +0200

    approach with wkn, still need termination

--- Reflection/TermsQIIT.lean
+++ Reflection/TermsQIIT.lean
@@ -1,5 +1,7 @@
 import Aesop
 import Reflection.Util.Vec
+import Mathlib.Tactic.Linarith
+import Mathlib.Tactic.Ring
 
 set_option linter.unusedVariables false
 set_option pp.fieldNotation.generalized false
@@ -18,17 +20,16 @@ mutual
   | U : ECon -> ETy
   /-- `El {Γ} : (t : Tm Γ U) -> Ty Γ` -/
   | El : ECon -> ETm -> ETy
-  /-- `Pi {Γ} : (A : Ty Γ) -> (B : Ty (ext Γ A)) -> Ty Γ` -/
+  /-- `Pi {Γ} : (A : Ty Γ) -> (B : Ty (Γ, A)) -> Ty Γ` -/
   | Pi : (Γ : ECon) -> (A : ETy) -> (B : ETy) -> ETy
-  -- | substTy : ETy -> ESubst -> ETy
   deriving Repr
 
   /-- `Var : (Γ : Con) -> Ty Γ -> Type` -/
   @[aesop unsafe constructors cases]
   inductive EVar : Type
-  /-- `vz {Γ} {A : Ty Γ} : Var (Γ, A) A[wk id]`, note that `wk id : (Γ, A) <- Γ`, thus `Γ, A ⊢ A[wk id]` -/
+  /-- `vz {Γ} {A : Ty Γ} : Var (Γ, A) A[wki]`, note that `wki : (Γ, A) <- Γ`, and `@wki Γ A = wkn (Γ, A) 1`, and `wki = wk id`. -/
   | vz : (Γ : ECon) -> (A : ETy) -> EVar
-  /-- `vs {Γ} {A B : Ty Γ} : Var Γ A -> Var (Γ, B) (substTy A wk)` -/
+  /-- `vs {Γ} {A B : Ty Γ} : Var Γ A -> Var (Γ, B) A[wki]`, but note that `wki` is a shorthand for `wkn (Γ, B) 1 : (Γ, B) <- Γ` -/
   | vs : (Γ : ECon) -> (A : ETy) -> (B : ETy) -> EVar -> EVar
   deriving Repr
 
@@ -37,20 +38,22 @@ mutual
   inductive ETm : Type
   /-- `var {Γ} {A : Ty Γ} : Var Γ A -> Tm Γ A` -/
   | var : (Γ : ECon) -> (A : ETy) -> EVar -> ETm
-  -- /-- `app {Γ} : {A : Ty Γ} -> {B : Ty (ext Γ A)} -> (f : Tm Γ (Pi A B)) -> (a : Tm Γ A) -> Tm Γ B[Var.vz ↦ a]`, here note that `Var.vz ↦ a` can be written as `Subst.cons Subst.id a`. -/
-  /-- `app {Γ} : {A : Ty Γ} -> {B : Ty (ext Γ A)} -> (f : Tm Γ (Pi A B)) -> (a : Tm Γ A) -> Tm Γ B[id, a]` -/
+  /-- `app {Γ} : {A : Ty Γ} -> {B : Ty (Γ, A)} -> (f : Tm Γ (Pi A B)) -> (a : Tm Γ A) -> Tm Γ B[id, a]`.\
+    Note that the substitution `(id, a) : Γ <- (Γ, A)` intuitively instantiates de-Brujin variable #0 with `a : Tm Γ A`.  -/
   | app : (Γ : ECon) -> (A : ETy) -> (B : ETy) -> (f : ETm) -> (a : ETm) -> ETm
-  /-- `lam {Γ} : {A : Ty Γ} -> {B : Ty (ext Γ A)} -> (body : Tm (ext Γ A) B) -> Tm Γ (Pi A B)` -/
+  /-- `lam {Γ} : {A : Ty Γ} -> {B : Ty (Γ, A)} -> (body : Tm (Γ, A) B) -> Tm Γ (Pi A B)` -/
   | lam : (Γ : ECon) -> (A : ETy) -> (B : ETy) -> (body : ETm) -> ETm
-  /-- necessary because of substVarE. Will be proven impossible in the final IIRT. -/
+  /-- Only necessary because of substVarE. Will be proven impossible in the final IIRT. -/
   | error : ETm
   deriving Repr
 
+  /-- A substitution `σ : Γ <- Δ` maps every variable in `Δ` to a `Γ`-term.
+    Intuitively, it is a list of length `Δ.length` storing terms typed in context `Γ`. -/
   @[aesop unsafe constructors cases]
   inductive ESubst : Type
-  /-- `Subst.nil : Subst Γ Con.nil` -/
+  /-- `Subst.nil {Γ} : Γ <- ⬝` -/
   | nil : (Γ : ECon) -> ESubst
-  /-- `cons : (δ : Subst Γ Δ) -> (t : Tm Γ (substTy A δ)) -> Subst Γ (Δ, A)` -/
+  /-- `Subst.cons {Γ} {Δ} {A : Ty Δ} : (δ : Γ <- Δ) -> (t : Tm Γ A[δ]) -> (Γ <- (Δ, A))` -/
   | cons : (Γ : ECon) -> (Δ : ECon) -> (A : ETy) -> ESubst -> ETm -> ESubst
   deriving Repr
 end
@@ -61,86 +64,117 @@ def substVarE : EVar -> ESubst -> ETm
 | .vs _ _ _ v, .cons _ _ _ σ _ => substVarE v σ
 | _, _ => .error
 
-mutual
-  @[simp] def ECon.size : ECon -> Nat
-  | .nil => 1
-  | .ext Γ A => 1 + Γ.size + A.size
+@[simp] def ECon.len : ECon -> Nat
+| .nil => 0
+| .ext Γ A => 1 + Γ.len
 
-  @[simp] def ETy.size : ETy -> Nat
-  | .U Δ => 1
-  | .El Δ t => 1 + t.size
-  | .Pi Δ A B => 1 + A.size + B.size
+/-- Drop the last `n` variables in the context. Example: `drop (⬝, A, B, C) 2 ≡ (⬝, A)`. -/
+@[simp] def ECon.drop : (Γ : ECon) -> Fin (Γ.len + 1) -> ECon
+| .nil    , n => .nil
+| .ext Γ A, ⟨0  , h⟩ => Γ
+| .ext Γ A, ⟨n+1, h⟩ => Γ.drop ⟨n, by rw [len] at h; linarith⟩
 
-  @[simp] def EVar.size : EVar -> Nat
-  | .vz .. => 1
-  | .vs _ _ _ v => 1 + v.size
+/-- Get the type of the de-Brujin variable `v`.
+   `get : (Γ : Con) -> (v : Fin Γ.len) -> Ty (drop Γ (v+1))`. -/
+@[simp] def ECon.get : (Γ : ECon) -> (v : Fin Γ.len) -> ETy
+| .nil    , v => Fin.elim0 v
+| .ext Γ A, ⟨0  , h⟩ => A
+| .ext Γ A, ⟨v+1, h⟩ => -- expected `Ty (drop (Γ, A) (v+1+1))`
+  -- ! theorem drop_ext : drop (Γ, A) (v+1+1) = drop Γ (v+1)
+  Γ.get ⟨v, by rw [len] at h; linarith⟩ -- : Ty (drop Γ (v+1))
 
-  @[simp] def ETm.size : ETm -> Nat
-  | .var _ A v => 1 + A.size + v.size
-  | .app _ A B f a => 1 + A.size + B.size + f.size + a.size
-  | .lam _ A B v => 1 + A.size + B.size + v.size
-  | .error => 0
+open ECon
 
-  @[simp] def ESubst.size : ESubst -> Nat
-  | .nil .. => 1
-  | .cons Γ Δ A σ a => 1 + A.size + σ.size + a.size
-end
+-- theorem (Γ : ECon) (n : Fin Γ.len) : sizeOf (Γ.get n) < sizeOf Γ
 
 mutual
-  /-- `substTy {Γ Δ : Con} (A : Ty Δ) (σ : Subst Γ Δ) : Ty Γ` -/
-  def substTyE (Γ Δ : ECon) : ETy -> ESubst -> ETy --:= fun A σ => match h : A , σ with
+  /-- `substTy {Γ Δ : Con} (A : Ty Δ) (σ : Γ <- Δ) : Ty Γ` -/
+  def substTyE (Γ Δ : ECon) : ETy -> ESubst -> ETy
   | .U Δ, σ => .U Γ
   | .El Δ t, σ => .El Γ (substTmE Γ Δ t σ)
   | .Pi Δ A B, σ => -- Δ ⊢ A
-    let A' : ETy /- Γ -/ := substTyE Γ Δ A σ -- Γ ⊢ A[σ]
-    let wk_σ : ESubst := wkE Γ Δ A' σ -- `wk σ : (Γ, A[σ]) <- Δ`, note that `wk σ = σ ∘ (wk id)`
-    have : sizeOf (wkE Γ Δ A' σ) = 0 := by simp_all [sizeOf]
-    -- * Terminates, because
-    let A'' : ETy := substTyE (.ext Γ A') Δ A wk_σ -- `A[wk σ] : (Γ, A[σ])`
-    let vz : ETm /- (Γ, A[σ]) A[wk σ] -/ := .var (.ext Γ A') A'' (.vz Γ A') -- `.vz Γ A' : Var (Γ, A[σ]) A[σ][wk id]`, note that `wk σ = σ ∘ (wk id)`
-    let δ : ESubst /- (Γ, A[σ]) <- (Δ, A) -/ := ESubst.cons (.ext Γ A') Δ A (wk_σ) vz
-    .Pi Γ A' (substTyE (.ext Γ A') (.ext Δ A) B δ)
-  termination_by A σ => A.size + σ.size
+    let Aσ : ETy /- Γ -/ := substTyE Γ Δ A σ -- Γ ⊢ A[σ]
+    let wk_σ /- : (Γ, A[σ]) <- Δ -/ := comp (.ext Γ Aσ) Γ Δ (wkn (.ext Γ Aσ) 1) σ -- note that `wk σ = (wkn (Γ, A[σ]) 1) ∘ σ`
+    let A_wk_σ : ETy := substTyE (.ext Γ Aσ) Δ A wk_σ -- `A[wk σ] : (Γ, A[σ])`
+    let vz : ETm /- (Γ, A[σ]) A[wk σ] -/ := .var (.ext Γ Aσ) A_wk_σ (.vz Γ Aσ) -- `.vz Γ A' : Var (Γ, A[σ]) A[σ][wk id]`, note that `wk σ = σ ∘ (wk id)`
+    let δ : ESubst /- (Γ, A[σ]) <- (Δ, A) -/ := ESubst.cons (.ext Γ Aσ) Δ A (wk_σ) vz
+    .Pi Γ Aσ (substTyE (.ext Γ Aσ) (.ext Δ A) B δ)
+  termination_by A σ => sizeOf Γ + sizeOf Δ + sizeOf A
 
   /-- `substTm {Γ Δ : Con} {A : Ty Δ} (t : Tm Δ A) (σ : Subst Γ Δ) : Tm Γ (substTy A σ)` -/
   def substTmE (Γ Δ : ECon) : ETm -> ESubst -> ETm
   | .var _ _ v, σ => substVarE v σ -- just pick the term in the subst that v refers to. if ill-formed, then.... uh... welp.
   | .app _Δ A B f a, σ => -- expected `Tm Γ B[id, a][σ]`
-    let A' : ETy /- Γ -/ := substTyE Γ Δ A σ -- Γ ⊢ A[σ]
-    let wk_σ : ESubst := wkE Γ Δ A' σ -- `wk σ : (Γ, A[σ]) <- Δ`, note that `wk σ = σ ∘ (wk id)`
-    let A'' : ETy /- (Γ, A[σ]) -/ := substTyE (.ext Γ A') Δ A wk_σ
-    let vz : ETm /- (Γ, A[σ]) A[wk σ] -/ := .var (.ext Γ A') A'' (.vz Γ A') -- `.vz Γ A' : Var (Γ, A[σ]) A[σ][wk id]`, note that `wk σ = σ ∘ (wk id)`
-    let δ : ESubst /- (Γ, A[σ]) <- (Δ, A) -/ := ESubst.cons (.ext Γ A') Δ A (wk_σ) vz
+    let Aσ : ETy /- Γ -/ := substTyE Γ Δ A σ -- Γ ⊢ A[σ]
+    -- let wk_σ : ESubst := wkE Γ Δ A' σ -- `wk σ : (Γ, A[σ]) <- Δ`, note that `wk σ = σ ∘ (wk id)`
+    let wk_σ : ESubst := comp (.ext Γ Aσ) Γ Δ (wkn (.ext Γ Aσ) 1) σ -- `wk σ : (Γ, A[σ]) <- Δ`, note that `wk σ = (wkn (Γ, A[σ]) 1) ∘ σ`
+    let A_wk_σ : ETy /- (Γ, A[σ]) -/ := substTyE (.ext Γ Aσ) Δ A wk_σ -- A[wk σ]
+    let vz : ETm /- (Γ, A[σ]) A[wk σ] -/ := .var (.ext Γ Aσ) A_wk_σ (.vz Γ Aσ) -- `.vz Γ A[σ] : Var (Γ, A[σ]) A[σ][wk id]`, note that `wk σ = (wk id) ∘ σ`
+    let δ : ESubst /- (Γ, A[σ]) <- (Δ, A) -/ := ESubst.cons (.ext Γ Aσ) Δ A (wk_σ) vz
 
-    let B' : ETy := substTyE (.ext Γ A') (.ext Δ A) B δ
+    let B' : ETy := substTyE (.ext Γ Aσ) (.ext Δ A) B δ
     let f' : ETm := substTmE Γ Δ f σ -- `f[σ] : Tm Γ (Pi A B)[σ]`, where `(Pi A B)[σ] = Pi A[σ] B[wk σ, #0]` per definition of substTy
     let a' : ETm := substTmE Γ Δ a σ -- `a[σ] : Tm Γ A[σ]`
-    let fa' : ETm := .app Γ A' B' f' a' -- `.app f[σ] a[σ] : Tm Γ B[wk σ, #0][id, a]`
+    let fa' : ETm := .app Γ Aσ B' f' a' -- `.app f[σ] a[σ] : Tm Γ B[wk σ, #0][id, a]`
     fa' -- ! here we need `((wk σ), #0) ∘ (id, a) = (id, a) ∘ σ` to typecheck.
   | .lam Δ A B body, σ => sorry
   | .error, _ => .error
-  termination_by t σ => t.size
+  termination_by t σ => sizeOf Γ + sizeOf Δ + sizeOf t
 
-  /-- `wk {Γ Δ : Con} {W : Ty Γ} : Subst Γ Δ -> Subst (Γ, W) Δ` -/
-  def wkE (Γ Δ : ECon) (W : ETy) : ESubst -> ESubst -- ! sizeOf wk is constant
-  | .nil Δ => .nil (.ext Γ W)
-  | .cons _ Δ A σ a => -- A : Ty Δ,   σ : Γ <- Δ,   a : Tm Γ A[σ],    expecting (Γ, W) <- (Δ, A)
-    let wk_σ := wkE Γ Δ W σ -- `wk σ : (Γ, W) <- Δ`
-    let A_wk_σ := substTyE (.ext Γ W) Δ A wk_σ -- `A[wk σ] : Ty (Γ, W)`
-    let a' : ETm := substTmE Γ (.ext Γ W) a (wkE Γ Γ W (idE Γ)) -- `a[wk id] : Tm (Γ, W) A[σ][wk id]`
-    let δ : ETm -> ESubst := .cons (.ext Γ W) Δ A wk_σ -- `δ : Tm (Γ, W) A[wk σ] -> ((Γ, W) <- (Δ, A))`
-    δ a' -- ! We use `σ ∘ (wk id) = wk σ` here (at the `a'`)
-  termination_by σ => σ.size
+  /-- `comp {Γ Θ Δ : Con} : Subst Θ Δ -> Subst Γ Θ -> Subst Γ Δ` -/
+  def comp (Γ Θ Δ : ECon) : ESubst -> ESubst -> ESubst
+  | .nil Θ, σ => .nil Γ
+  | .cons Θ Δ A δ t, σ => -- `δ : Θ <- Δ`,   `σ : Γ <- Θ`,   `Θ ⊢ t : A[δ]`,   expected `Γ <- Δ, A`
+    .cons Γ Δ A
+      (comp Γ Θ Δ δ σ) -- δ ∘ σ : Γ <- Δ
+      (substTmE Γ Θ t σ) -- `Γ ⊢ t[σ] : A[δ][σ]`, -- ! need theorem `A[δ][σ] = A[δ ∘ σ]`
+  termination_by δ σ => sizeOf Γ + sizeOf Θ + sizeOf Δ + sizeOf δ
 
-  /-- `id {Γ : Con} : Subst Γ Γ` -/
-  def idE : (Γ : ECon) -> ESubst
-  | .nil => .nil .nil -- `Subst .nil .nil`
-  | .ext Γ A => -- expecting `(Γ, A) <- (Γ, A)`
-    let wk1 := wkE Γ Γ A (idE Γ) -- `wk1 : (Γ, A) <- Γ`
-    .cons (.ext Γ A) Γ A -- `cons (Γ, A) Γ A : (δ : (Γ, A) <- Γ) -> (t : Tm (Γ, A) A[δ]) -> ((Γ, A) <- (Γ, A))`
-      wk1 -- `cons (Γ, A) Γ A wk1 : (t : Tm (Γ, A) A[wk]) -> Subst (Γ, A) (Γ, A)`
-      (.var (.ext Γ A) (substTyE (.ext Γ A) Γ A wk1) (.vz Γ A))
-  termination_by Γ => Γ.size
+  /-- `wkn {Γ : Con} (n : Fin (Γ.len + 1)) : (Γ <- Γ - n)` -/
+  def wkn (Γ : ECon) (n : Fin (Γ.len + 1)) : ESubst :=
+    if h : Γ.len = n then .nil Γ
+    else
+      have h : n < Γ.len := by
+        have h₁ : n.val < Γ.len + 1 := n.2
+        have h₂ : ¬ n.val = Γ.len := by aesop
+        sorry
+      -- have : sizeOf Γ < ECon.len Γ - ↑n := sorry
+      have : Γ.len >= n := by sorry -- we don't have `Nat` subtraction "underflow".
+      have : Γ.len - (↑n + 1) < Γ.len - ↑n := by sorry -- why does linarith fail here
+      have : ECon.len Γ - n < ECon.len Γ := by sorry
+      -- have : ECon.len Γ + ↑n < ECon.len Γ + ECon.len Γ - ↑n := by linarith
+      ESubst.cons Γ (Γ.drop n)
+        (Γ.get ⟨n, by simp only [h]⟩) -- Γₙ
+        (wkn Γ ⟨n+1, by linarith⟩) -- `wkn Γ (n+1) : Γ <- Γ - (n+1)`
+        (.var Γ
+          -- * for termination here we need `sizeOf (Γ.get n) < sizeOf Γ` (not sure about this)
+          (substTyE Γ (Γ.drop (n+1)) (Γ.get ⟨n, by simp only [h]⟩) (wkn Γ ⟨n+1, by linarith⟩)) -- `Γᵥ[wki]`
+          -- ?    Γ.len +     <     Γ.len + Γ.len - (n+1)
+          (mkVar Γ ⟨n, by linarith⟩)
+        )
+  termination_by sizeOf Γ + (Γ.len - n.val)
+
+  -- `mkVar : (Γ : Con) -> (v : Fin Γ.len) -> Var Γ (Γ.get v)[wkn Γ (v+1)]`
+  def mkVar : (Γ : ECon) -> (v : Fin Γ.len) -> EVar
+  | .nil, v => Fin.elim0 v
+  | .ext Γ X, ⟨0  , _⟩ => -- expected `Var (Γ, X) (get (Γ, X) 0)[wkn (Γ, X) (0 + 1)]`
+    -- by defeq we have `get (Γ, X) 0 ≡ X`
+    .vz Γ X -- `: Var (Γ, X) X[wki]`, where `wki` is just a shorthand for `wkn (Γ, X) 1`.
+  | .ext Γ X, ⟨v+1, h⟩ => -- expected `Var (Γ, X) (get (Γ, X) (v+1))[wkn (Γ, X) (v + 1 + 1)]`
+    -- ! need theorem get_ext : get (Γ, X) (v+1) = get Γ v
+    -- ! need theorem : wkn Γ (v+1) ∘ wkn (Γ, X) 1 = wkn (Γ, X) (v+1+1)
+    .vs
+      Γ
+      (substTyE
+        Γ
+        (Γ.drop ⟨v+1, by rw [ECon.len] at h; linarith⟩)
+        (Γ.get ⟨v, by rw [ECon.len] at h; linarith⟩) -- `Γᵥ`
+        -- ?    len (Γ, X) + len (Γ,X) - (v+1+1)    <    len (Γ, X) + ??
+        (wkn (.ext Γ X) ⟨v+1+1, by simp_all only [len, add_lt_add_iff_right]⟩) -- `wkn (Γ, X) (v+1+1)`
+      ) -- `Γᵥ[wkn (Γ, X) (v+1+1)]`
+      X
+      (mkVar Γ ⟨v, by rw [ECon.len] at h; linarith⟩) -- `mkVar Γ v : Var Γ (Γ.get v)[wkn Γ (v+1)]`
+  termination_by Γ v => sizeOf Γ
 end
 
 #exit
