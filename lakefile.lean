9529ceaa171087ec598417c75b7f60153123de2d
arthur-adjedj <arthur.adjedj@gmail.com>
2024-10-30 11:13:08 +0000

    chore: bump to 4.12.0

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,12 @@ package «reflection» {
   -- add package configuration options here
 }
 
--- require aesop from git "https://github.com/leanprover-community/aesop" -- 01cb707de69cf88733addfff0bacc710efae8d20
-require std from git "https://github.com/leanprover/std4" @ "v4.5.0-rc1"
-require aesop from git "https://github.com/JLimperg/aesop" @ "v4.5.0-rc1"
+-- require std from git "https://github.com/leanprover/std4" @ "nightly-testing-2024-04-04"
+require aesop from git "https://github.com/JLimperg/aesop" @ "v4.12.0"
+-- require IIT from git "https://github.com/arthur-adjedj/iit" @ "v4.4.0"
+-- require Qq from git "https://github.com/leanprover-community/quote4" @ "53156671405fbbd5402ed17a79bd129b961bd8d6"
+-- require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
+-- require auto from git "https://github.com/leanprover-community/lean-auto" @ "v4.11.0" -- doesn't work from lean 4.8
 
 @[default_target]
 lean_lib «Reflection» {
